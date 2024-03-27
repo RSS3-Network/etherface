d3464efa1cc200daf4983d087af095c6535cbecd
incubator4 <aries0robin@gmail.com>
2024-03-27 18:59:54 +0800

    chore(rest): update bind addr

--- etherface-rest/src/main.rs
+++ etherface-rest/src/main.rs
@@ -6,13 +6,7 @@ use actix_web::web;
 use actix_web::App;
 use actix_web::HttpServer;
 use etherface_lib::database::handler::DatabaseClientPooled;
-use openssl::ssl::SslAcceptor;
-use openssl::ssl::SslFiletype;
-use openssl::ssl::SslMethod;
 use v1::AppState;
-
-const PATH_PRIVATE_KEY: &str = "/etc/letsencrypt/live/api.etherface.io/privkey.pem";
-const PATH_CERTIFICATE: &str = "/etc/letsencrypt/live/api.etherface.io/fullchain.pem";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,8 +15,6 @@ async fn main() -> std::io::Result<()> {
     let state = web::Data::new(AppState {
         dbc: DatabaseClientPooled::new().unwrap(),
     });
-
-    let addrs = "0.0.0.0:8080";
 
     HttpServer::new(move || {
         App::new().app_data(state.clone()).service(
@@ -36,7 +28,7 @@ async fn main() -> std::io::Result<()> {
                 .wrap(Logger::new("(%Ts, %s) %a: %r").log_target("v1::logger")),
         )
     })
-    .bind(addrs)
+    .bind(("0.0.0.0", 8080))
     .unwrap()
     .run()
     .await
